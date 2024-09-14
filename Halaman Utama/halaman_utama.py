37df051b9a699a784159878764678ab3550251db
LaboNapitupulu <labo.123450037@student.itera.ac.id>
2024-09-15 00:34:52 +0700

    deskripsi kelompok

--- Halaman Utama/halaman_utama.py
+++ Halaman Utama/halaman_utama.py
@@ -102,24 +102,19 @@ if menu == "Home":
             "<h1 class='centered-title'>Deskripsi Kelompok</h1>", unsafe_allow_html=True
         )
         st.markdown(
-            """<div style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est 
-                    laborum.</div>""",
+            """<div style="text-align: justify;">Uniform adalah kelompok kaderisasi HMSD Adyatama ke-9. Nama "Uniform" berasal dari distribusi seragam dalam statistika, 
+            di mana setiap hasil memiliki peluang yang sama untuk terjadi. Ini mencerminkan prinsip kesetaraan dan keadilan, 
+            dengan kata lain setiap anggota kelompok memiliki peran yang sama pentingnya, 
+            dan setiap ide serta kontribusi dianggap setara..</div>""",
             unsafe_allow_html=True,
         )
         st.write(""" """)
         foto_kelompok = "https://drive.google.com/uc?export=view&id=1Rnu9pJsPjzRTbWu901GfM0dxFFRp3os0"
         layout(foto_kelompok)
         st.markdown(
-            """<div style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est 
-                    laborum.</div>""",
+            """<div style="text-align: justify;">Meski setiap anggota memiliki peran yang berbeda, 
+            semua bersatu dalam satu tujuan, seperti halnya distribusi seragam yang memberikan peluang yang sama untuk setiap nilai. Ini menekankan pentingnya kerja sama dan sinergi dalam mencapai hasil yang optimal dalam sebuah tim. Meskipun kami adalah kelompok terakhir, 
+            tapi kami percaya dengan kata=kata "Save the best for the last". This is Uniform. Unity of Many Form.</div>""",
             unsafe_allow_html=True,
         )
         st.write(""" """)
