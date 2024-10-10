5e824130ae5a1dc571d259dee8c0a0e84e14a877
LaboNapitupulu <labo.123450037@student.itera.ac.id>
2024-10-10 21:20:01 +0700

    ulang

--- Buku Kating/037_Labo Napitupulu.py
+++ Buku Kating/037_Labo Napitupulu.py
@@ -20,10 +20,8 @@ def streamlit_menu():
             "Departemen Eksternal",
             "Departemen Internal",
             "Departemen SSD",
-            "Departemen MEDKRAF",
         ],
         icons=[
-            "people-fill",
             "people-fill",
             "people-fill",
             "people-fill",
