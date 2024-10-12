bd15257c490ea44a50cf069c7d6da9419eb49cdc
DeaAmanda98 <dea.123450006@student.itera.ac.id>
2024-10-13 00:19:20 +0700

    Update 006_Dea Amanda.py

--- Buku Kating/006_Dea Amanda.py
+++ Buku Kating/006_Dea Amanda.py
@@ -180,7 +180,7 @@ if menu == "Kesekjenan":
 elif menu == "Baleg":
     def baleg():
         gambar_urls = [
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
+            "https://drive.google.com/uc?export=view&id=1nSY14MqzQHxPynNSKFD8853iwG4z8cSx",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
         ]
