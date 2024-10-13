51b54c334482d3e2c3b53ed0c57da6f067930504
arielvaa <151176785+arielvaa@users.noreply.github.com>
2024-10-13 17:28:12 +0700

    Update 062_Abdillah Fikri Al Pome.py

--- Buku Kating/062_Abdillah Fikri Al Pome.py
+++ Buku Kating/062_Abdillah Fikri Al Pome.py
@@ -99,7 +99,7 @@ menu = streamlit_menu()
 if menu == "Kesekjenan":
     def kesekjenan():
         gambar_urls = [
-            "https://drive.google.com/uc?export=view&id=1c4vPah1Xk96YFEEP7WWAYdlH3lxi_mR7",
+            "https://drive.google.com/uc?export=view&id=1jml24USid6KLJZJA7Ijz1YMg9_KaVycO",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
