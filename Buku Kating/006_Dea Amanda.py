d9c34b944af3c7c910745bae8dc727b89ebfe682
DeaAmanda98 <dea.123450006@student.itera.ac.id>
2024-10-13 10:10:27 +0700

    baleg

--- Buku Kating/006_Dea Amanda.py
+++ Buku Kating/006_Dea Amanda.py
@@ -180,9 +180,17 @@ if menu == "Kesekjenan":
 elif menu == "Baleg":
     def baleg():
         gambar_urls = [
-            "https://drive.google.com/uc?export=view&id=1nSY14MqzQHxPynNSKFD8853iwG4z8cSx",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
+            "https://drive.google.com/uc?export=view&id=1nSY14MqzQHxPynNSKFD8853iwG4z8cSx", #1
+            "https://drive.google.com/uc?export=view&id=1mxJV6FjKUdVouRYPw88IGnXOKoEYLrQj", #2
+            "https://drive.google.com/uc?export=view&id=1mxKBggEQbDG4LYK1O0-VS210Liwj-U5q", #3
+             "https://drive.google.com/uc?export=view&id=1mWNCuF2SJ7KfIw-UBjaZCelCsyJv9PLe", #4
+            "https://drive.google.com/uc?export=view&id=1moiwRn5OR6y7BijrqRj3z513vOfDcTkC", #5
+             "https://drive.google.com/uc?export=view&id=1niPK785_CrjFvm-GTj0B03C9Hu8RhLbl", #6
+            "https://drive.google.com/uc?export=view&id=1o5nSdoGXWr51fqD1NgaJdcBwnmjIaqUw", #7
+             "https://drive.google.com/uc?export=view&id=1oMfVYj022xtRTo-MEwPCrTHtlq9dnAcu", #8
+            "https://drive.google.com/uc?export=view&id=1ocf3o5So7sdopZiaDYsB1D2gYMg6Lcya", #9
+             "https://drive.google.com/uc?export=view&id=15MMrZP6G-iY2uR5kPBb41yWpNIUe9ZzV", #10
+            "https://drive.google.com/uc?export=view&id=1nKPNka6scb3ZFWUZ3e_CcmIHmlvm2PLo", #11
         ]
         data_list = [
             {
