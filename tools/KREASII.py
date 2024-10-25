f123ad8a080289c392b10d4726a61b5fead23658
LaboNapitupulu <labo.123450037@student.itera.ac.id>
2024-10-25 14:00:56 +0700

    Update KREASII.py

--- tools/KREASII.py
+++ tools/KREASII.py
@@ -1,10 +1,1 @@
-import streamlit as st
 
-# URL gambar dari Google Drive (gunakan direct link Google Drive)
-image_url = "https://drive.google.com/uc?export=view&id=1uTojWTp3nOwNQWYiRuXjHlFcFlfyfWuM"
-
-# Judul aplikasi
-st.title("Tes Gambar Mobil dari Google Drive")
-
-# Menampilkan gambar
-st.image(image_url, caption="Gambar Mobil", use_column_width=True)
