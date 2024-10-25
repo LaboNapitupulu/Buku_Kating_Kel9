9c03020f2051673949ff58ee024fc76a3d31f0e9
LaboNapitupulu <labo.123450037@student.itera.ac.id>
2024-10-25 14:07:03 +0700

    Update KREASII.py

--- tools/KREASII.py
+++ tools/KREASII.py
@@ -1,15 +1,1 @@
-import streamlit as st
-from PIL import Image
-from io import BytesIO
-import requests
 
-
-image_url = "https://drive.google.com/uc?export=download&id=1uTojWTp3nOwNQWYiRuXjHlFcFlfyfWuM"
-
-response = requests.get(image_url)
-
-if response.status_code == 200:
-    image = Image.open(BytesIO(response.content))
-    st.image(image, caption="Gambar Mobil dari Google Drive", use_column_width=True)
-else:
-    st.error("Gambar tidak bisa diambil dari URL. Status code: {}".format(response.status_code))
