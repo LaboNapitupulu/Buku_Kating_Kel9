eab8b30f0e8700b1bcdd82841f988f2e6f4a76b1
Lutfiaaisyah <lutfiaaisyahputri28@gmail.com>
2024-11-04 09:17:27 +0700

    bsg

--- Buku Kating/074_Lutfia Aisyah Putri.py
+++ Buku Kating/074_Lutfia Aisyah Putri.py
@@ -769,7 +769,7 @@ elif menu == "Departemen Eksternal":
         "https://drive.google.com/uc?export=view&id=1T6QfBqkXm3Bv3J7RzJNxC5VuWhBkQIc1", #Kak Izza 
         "https://drive.google.com/uc?export=view&id=19RTYQHrVSfdJ5RWGIXUigYccXV2b7vn_", #Kak Khaalishah 
         "https://drive.google.com/uc?export=view&id=1193xrrIUl-K3vpbJvB_4XjQbA8ZGzCKw", #kak Yuna
-        "https://drive.google.com/uc?export=view&id=1uKsVIbDc8dizbcdN2khTZoQwAjv4NBPU", #Bang Raid
+        "https://drive.google.com/uc?export=view&id=1T4rgr0JVMEgcPEjPPI4-UTTsXWNZD9aR", #Bang Raid
     
         ]
         data_list = [
